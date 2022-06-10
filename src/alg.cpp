db41c13fa418169763b26efee64dfa3ab3fe1a14
Матвеев Артем <99340742+MATVEEVru@users.noreply.github.com>
2022-06-10 23:02:12 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -11,9 +11,7 @@ bool checkPrime(uint64_t value) {
     }
   return true;
 }
-
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
     int count = 0;
     int value = 1;
     while (count != n) {
@@ -24,9 +22,7 @@ uint64_t nPrime(uint64_t n) {
     }
   return value;
 }
-
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
     int v = value+1;
     while (true) {
     if (checkPrime(v))
@@ -35,9 +31,7 @@ uint64_t nextPrime(uint64_t value) {
   }
   return v;
 }
-
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
     uint64_t summ = 0;
     while (hbound !=2) {
         hbound--;
